48753a9a18cb753e5aadaac57d026f8e3f0dde84
ivanp___ <rafa.pejovic@gmail.com>
2023-08-01 21:36:52 +0200

    Napravio tabelu sa izmijenjenim asp.net identity nalozima i preveo poruke na crnogorski jezik

--- Videoteka/Models/AccountViewModels.cs
+++ Videoteka/Models/AccountViewModels.cs
@@ -29,11 +29,11 @@ namespace Videoteka.Models
         public string Provider { get; set; }
 
         [Required]
-        [Display(Name = "Code")]
+        [Display(Name = "Kod")]
         public string Code { get; set; }
         public string ReturnUrl { get; set; }
 
-        [Display(Name = "Remember this browser?")]
+        [Display(Name = "Zapamti browser?")]
         public bool RememberBrowser { get; set; }
 
         public bool RememberMe { get; set; }
@@ -58,7 +58,7 @@ namespace Videoteka.Models
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Zapamti me?")]
         public bool RememberMe { get; set; }
     }
 
@@ -70,15 +70,23 @@ namespace Videoteka.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} mora imati barem {2} karaktera.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Potvrdi password")]
+        [Compare("Password", ErrorMessage = "Password i Potvrdi password se ne poklapaju.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Adresa je obavezno polje")]
+        public string Adresa { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Adresa je obavezno polje")]
+        public string Telefon { get; set; }
     }
 
     public class ResetPasswordViewModel
@@ -89,14 +97,14 @@ namespace Videoteka.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} mora imati barem {2} karaktera.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessage = "Password i Potvrdi password se ne poklapaju.")]
         public string ConfirmPassword { get; set; }
 
         public string Code { get; set; }
