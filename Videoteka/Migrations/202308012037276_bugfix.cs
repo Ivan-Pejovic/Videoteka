99161d8e42795607f37c8a6876ff5b6656fc7b88
ivanp___ <rafa.pejovic@gmail.com>
2023-08-01 22:40:10 +0200

    Updateovao navbar i updateovao restrikciju za duzinu naziva u tabelama

---
+++ Videoteka/Migrations/202308012037276_bugfix.cs
@@ -0,0 +1,20 @@
+﻿namespace Videoteka.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class bugfix : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.TipClanstvas", "Naziv", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.TipKupcas", "Naziv", c => c.String(nullable: false, maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.TipKupcas", "Naziv", c => c.String(nullable: false, maxLength: 100));
+            AlterColumn("dbo.TipClanstvas", "Naziv", c => c.String(nullable: false, maxLength: 100));
+        }
+    }
+}
