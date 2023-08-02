2e05061a13ec1ecaa796623cd267aaf5f1349677
ivanp___ <rafa.pejovic@gmail.com>
2023-08-02 14:45:06 +0200

    Napravio formu za pravljenje novog i izmjenu postojeceg kupca kao i odgovarajuce metode u controlleru i api-s

--- Videoteka/Controllers/KupciController.cs
+++ Videoteka/Controllers/KupciController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Videoteka.Models;
+using Videoteka.ViewModels;
 
 namespace Videoteka.Controllers
 {
@@ -24,6 +25,73 @@ namespace Videoteka.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Novi()
+        {
+            var tipoviKupca = _context.TipoviKupaca.ToList();
+            var tipoviClanstva = _context.TipoviClanstva.ToList();
+            var viewModel = new KupacViewModel
+            {
+                Kupac = new Kupac(),
+                TipoviKupaca = tipoviKupca,
+                TipoviClanstva = tipoviClanstva
+            };
+
+            return View("KupacForma", viewModel);
+        }
+
+        public ActionResult Izmijeni(int id)
+        {
+            var kupac = _context.Kupci.SingleOrDefault(k => k.Id == id);
+
+            if(kupac == null)
+                return HttpNotFound();
+
+            var viewModel = new KupacViewModel
+            {
+                Kupac = kupac,
+                TipoviKupaca = _context.TipoviKupaca.ToList(),
+                TipoviClanstva = _context.TipoviClanstva.ToList()
+            };
+
+            return View("KupacForma", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Kupac kupac)
+        {
+            if(!ModelState.IsValid)
+            {
+                var viewModel = new KupacViewModel
+                {
+                    Kupac = kupac,
+                    TipoviKupaca = _context.TipoviKupaca.ToList(),
+                    TipoviClanstva = _context.TipoviClanstva.ToList()
+                };
+
+                return View("KupacForma", viewModel);
+            }
+
+            if(kupac.Id == 0)
+            {
+                _context.Kupci.Add(kupac);
+            }
+            else
+            {
+                var kupacInDb = _context.Kupci.Single(k => k.Id == kupac.Id);
+
+                kupacInDb.Ime = kupac.Ime;
+                kupacInDb.PrimaObavjestenja = kupac.PrimaObavjestenja;
+                kupacInDb.DatumRodjenja = kupac.DatumRodjenja;
+                kupacInDb.TipKupcaId = kupac.TipKupcaId;
+                kupacInDb.TipClanstvaId = kupac.TipClanstvaId;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Kupci");
         }
     }
 }
---
+++ Videoteka/ViewModels/KupacViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Videoteka.Models;
+
+namespace Videoteka.ViewModels
+{
+    public class KupacViewModel
+    {
+        public Kupac Kupac { get; set; }
+        public IEnumerable<TipKupca> TipoviKupaca { get; set; }
+        public IEnumerable<TipClanstva> TipoviClanstva { get; set; }
+    }
+}
