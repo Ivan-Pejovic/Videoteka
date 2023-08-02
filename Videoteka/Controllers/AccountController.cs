1328828ba93c0a31a48279cf3ee7c0c75ec2bcc0
ivanp___ <rafa.pejovic@gmail.com>
2023-08-02 23:59:56 +0200

    Account registration bugfix

--- Videoteka/Controllers/AccountController.cs
+++ Videoteka/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace Videoteka.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Telefon = model.Telefon, Adresa = model.Adresa };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
